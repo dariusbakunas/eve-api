496a05eba2196c4fc30a2f24707d30231e0f0427
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-04 19:01:52 -0400

    add coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+  ],
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
