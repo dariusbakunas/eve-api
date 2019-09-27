4d91222bd891368e7bd0388e0a7aec79d18b189b
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-27 08:59:18 -0400

    add eslint/prettier

---
+++ prettierrc.js
@@ -0,0 +1,14 @@
+const config = {
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 150,
+  tabWidth: 4
+};
+
+export default config;
