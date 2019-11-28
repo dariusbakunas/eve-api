8cd40a58457b31be92db59714ac72e5882689c77
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-11-27 19:47:38 -0500

    make sure we're using utc tz

--- knexfile.ts
+++ knexfile.ts
@@ -6,6 +6,14 @@ module.exports = {
       database: process.env.DEV_DB_DATABASE,
       user: process.env.DEV_DB_USERNAME,
       password: process.env.DEV_DB_PASSWORD,
+      timezone: '+00:00',
+    },
+    pool: {
+      afterCreate: function(connection: any, callback: (err: any, connection: any) => void) {
+        connection.query("SET time_zone='+00:00';", function(err: any) {
+          callback(err, connection);
+        });
+      },
     },
     migrations: {
       directory: __dirname + '/src/services/db/migrations',
@@ -22,10 +30,16 @@ module.exports = {
       database: process.env.PD_DB_DATABASE,
       user: process.env.PD_DB_USERNAME,
       password: process.env.PD_DB_PASSWORD,
+      timezone: '+00:00',
     },
     pool: {
       min: 2,
       max: 10,
+      afterCreate: function(connection: any, callback: (err: any, connection: any) => void) {
+        connection.query("SET time_zone='+00:00';", function(err: any) {
+          callback(err, connection);
+        });
+      },
     },
     migrations: {
       directory: __dirname + '/src/services/db/migrations',
