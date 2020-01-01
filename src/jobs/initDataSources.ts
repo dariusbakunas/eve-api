9fa197356d0e81e9e639a98f2c067bf5a21740f3
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-01-01 12:23:36 -0500

    fix

--- src/jobs/initDataSources.ts
+++ src/jobs/initDataSources.ts
@@ -1,10 +1,17 @@
 import { InMemoryLRUCache } from 'apollo-server-caching';
+import { Model } from 'objection';
 import Crypt from '../services/crypt';
 import db from '../services/db';
 import EsiAPI from '../services/esi/api';
 import EsiAuth from '../services/esi/auth';
+import Knex from 'knex';
 
 export const initDataSources = () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const dbConfig = require('../knexfile');
+  const knex = Knex(dbConfig[process.env.NODE_ENV!]);
+  Model.knex(knex);
+
   const dataSources = {
     esiAuth: new EsiAuth(process.env.EVE_LOGIN_URL!),
     esiApi: new EsiAPI(process.env.EVE_ESI_URL!),
