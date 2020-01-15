549815da200f98f800a0ee1aa0c26a0ba3367b34
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-01-14 20:55:01 -0500

    remove old jobs
    moved to eve-processing project

--- src/jobs/esiProcess.ts
+++ src/jobs/esiProcess.ts
@@ -3,14 +3,8 @@ import { cleanup } from './cleanup';
 import { getAccessToken } from '../resolvers/common';
 import { initDataSources } from './initDataSources';
 import { processAlliances } from './processAlliances';
-import { processBlueprints } from './processBlueprints';
 import { processBookmarks } from './processBookmarks';
-import { processCharacterMarketOrders } from './processCharacterMarketOrders';
 import { processCorporations } from './processCorporations';
-import { processJournalEntries } from './processJournalEntries';
-import { processSkillQueue } from './prrocessSkillQueue';
-import { processSkills } from './processSkills';
-import { processWalletTransactions } from './processWalletTransactions';
 import { updateNameCache } from './updateNameCache';
 import logger from '../utils/logger';
 
@@ -25,26 +19,8 @@ export const processData = async () => {
     try {
       const token = await getAccessToken(id, accessToken, refreshToken, expiresAt, db, crypt, esiAuth);
 
-      if (scopes.includes('esi-wallet.read_character_wallet.v1')) {
-        await processWalletTransactions(character, token, db, esiApi);
-        await processJournalEntries(character, token, db, esiApi);
-      }
-
       if (scopes.includes('esi-bookmarks.read_character_bookmarks.v1')) {
         await processBookmarks(character, token, db, esiApi);
-      }
-
-      if (scopes.includes('esi-markets.read_character_orders.v1')) {
-        await processCharacterMarketOrders(character, token, db, esiApi);
-      }
-
-      if (scopes.includes('esi-skills.read_skills.v1')) {
-        await processSkills(character, token, db, esiApi);
-        await processSkillQueue(character, token, db, esiApi);
-      }
-
-      if (scopes.includes('esi-characters.read_blueprints.v1')) {
-        await processBlueprints(character, token, db);
       }
     } catch (e) {
       logger.error(e);
