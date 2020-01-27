db31d260d229a762e06fbd32492d1afd70b9c6ae
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-01-26 19:57:08 -0500

    add staging db config

--- knexfile.ts
+++ knexfile.ts
@@ -67,4 +67,31 @@ module.exports = {
     },
   },
   production: productionConfig,
+  staging: {
+    client: 'mysql2',
+    pool: {
+      min: 2,
+      max: 10,
+      afterCreate: function(connection: any, callback: (err: any, connection: any) => void) {
+        connection.query("SET time_zone='+00:00';", function(err: any) {
+          callback(err, connection);
+        });
+      },
+    },
+    migrations: {
+      directory: __dirname + '/src/services/db/migrations',
+      tableName: 'knex_migrations',
+      extension: 'ts',
+    },
+    connection: {
+      host: process.env.STAGING_DB_HOST,
+      database: process.env.STAGING_DB_DATABASE,
+      user: process.env.STAGING_DB_USERNAME,
+      password: process.env.STAGING_DB_PASSWORD,
+      timezone: '+00:00',
+    },
+    seeds: {
+      directory: __dirname + '/src/services/db/seeds',
+    },
+  },
 };
